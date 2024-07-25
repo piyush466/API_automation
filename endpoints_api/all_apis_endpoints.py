550a1d508f2cf210496c9adffa72fde5d2cfe1f2
piyush dravyakar <piyushdravyakar69@gmail.com>
2024-07-25 10:30:52 +0530

    add delete api test

--- endpoints_api/all_apis_endpoints.py
+++ endpoints_api/all_apis_endpoints.py
@@ -1,4 +1,6 @@
 class Endpoints:
+    BASE_URL = "https://reqres.in/api"
 
-    SINGLE_USER_API = "/users/2"
-    CREATE_NEW_USER = "/users"
+    URL_SINGLE_USER_API = BASE_URL+"/users/2"
+    URL_CREATE_NEW_USER = BASE_URL+"/users"
+    URL_UPDATE_EXIST_USER = BASE_URL+"/users/2"
--- tests/test_01_single_user_data.py
+++ tests/test_01_single_user_data.py
@@ -9,24 +9,25 @@ from data.payloads.create_new_user_data import Payload
 class Test_API:
 
     def test_01_get_single_userdata(self):
-        response = APIrequests.get_api(f"{BASE_URL}{Endpoints.SINGLE_USER_API}")
+        response = APIrequests.get_api(f"{Endpoints.URL_SINGLE_USER_API}")
         print(response.json())
         Json_reader.validate_schema("valid_getApi_single_user_schema.json", response.json())
-        id = type(response.json()['data']['id'])
-        assert id == int
 
     def test_02_post_new_user(self):
         payload = Payload.new_user("piyush","QA Automation Tester")
-        response = APIrequests.post_api(f"{BASE_URL}{Endpoints.CREATE_NEW_USER}", payload)
+        response = APIrequests.post_api(f"{Endpoints.URL_CREATE_NEW_USER}", payload)
         print(response.json())
         Json_reader.validate_schema("valid_postApi_create_new_user_schema.json", response.json())
-        assert type(response.json()['name']) == str
         assert response is not None
         assert response.status_code == 201
 
     def test_03_put_data(self):
         payload = Payload.update_user_details("Pravin", "QA tester")
-        response = APIrequests.put_api(f'{BASE_URL}/users/2', payload)
-        print(response.json())
+        response = APIrequests.put_api(f'{Endpoints.URL_UPDATE_EXIST_USER}', payload)
+        Json_reader.validate_schema("valid_putApi_update_data_schema.json", response.json())
         assert response.json()['name'] == "Pravin"
         assert response.status_code == 200
+
+    def test_04_delete_data(self):
+        response = APIrequests.delete_api('https://reqres.in/api/users/2')
+        assert response.status_code == 204
